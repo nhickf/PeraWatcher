d5c84112c0437e537bc2e441b15e26f73086f4f5
Nhick Flores <48816190+nhickf@users.noreply.github.com>
2022-06-20 23:42:47 +0800

    Add clearstack in adding wallet and navigate up

--- app/src/main/java/com/creativegrpcx/perawatcher/dependencyInjector/ApplicationModule.kt
+++ app/src/main/java/com/creativegrpcx/perawatcher/dependencyInjector/ApplicationModule.kt
@@ -2,6 +2,10 @@ package com.creativegrpcx.perawatcher.dependencyInjector
 
 import android.app.Application
 import android.text.Editable
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.creativegrpcx.perawatcher.BaseActivity
+import com.creativegrpcx.perawatcher.R
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
